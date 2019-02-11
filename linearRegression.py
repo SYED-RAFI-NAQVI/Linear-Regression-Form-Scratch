79fe551edad2854818658132281b6d2c75234cc7
Syed-Rafi-Naqvi <syedrafinaqvi@SYEDs-MacBook-Pro.local>
2019-02-12 00:17:08 +0530

    data and main file(linearRegression.py) added

---
+++ linearRegression.py
@@ -0,0 +1,18 @@
+#importing the required packages
+import numpy as np
+
+
+
+def run():
+    #step 1 gathering the data
+    points = np.genfromtxt('data.csv', delimiter=',')
+
+    #step 2 required parameters
+    learning_rate = 0.001
+    init_b = 0
+    init_m = 0
+    num_iterations = 1000
+
+
+def main():
+    run()
