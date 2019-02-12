204e103acdfb1c1b0585273dd5cf008f3239af25
Syed-Rafi-Naqvi <syedrafinaqvi@SYEDs-MacBook-Pro.local>
2019-02-12 14:17:07 +0530

    error line function is added in linearRegression.py

--- linearRegression.py
+++ linearRegression.py
@@ -1,17 +1,27 @@
 #importing the required packages
 import numpy as np
 
+def error_of_line(b, m, points):
+    totalError = 0
+    for i in range(0, len(points)):
+        x =  points[i, 0]
+        y = points[i, 1]
+        totalError += (y- (m * x + b)) ** 2
+        return totalError/ float(len(points))
 
 
 def run():
-    #step 1 gathering the data
+    #Step 1 gathering the data
     points = np.genfromtxt('data.csv', delimiter=',')
 
-    #step 2 required parameters
+    #Step 2 required parameters
     learning_rate = 0.001
     init_b = 0
     init_m = 0
     num_iterations = 1000
+    #Step 3 finding the error for the intial line or intial gradient.
+    print("intial gradient decent at b = {0}, m = {1}, error = {2}".format(init_b, init_m, error_of_line(init_b, init_m, points)))
+
 
 
 def main():
